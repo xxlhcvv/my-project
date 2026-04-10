2f17a4144f8bc8de5c1efe4bf606832b896f3f79
xxlhcvv <2407978636@qq.com>
2026-04-10 14:32:51 +0800

    删除my-project中重复的Python项目文件

--- python3.12第二课/3.26.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-df=pd.read_csv(r'D:\AI\第3章\data\data\used_cars.csv',sep=',',header=0)
-columns=['brand','bodyType','fuelType','gearbox','power','kilometer','notRepairedDamage','days','v_0','v_1','price']
-df=df[columns][:]
-df=(df-df.min())/(df.max()-df.min())
